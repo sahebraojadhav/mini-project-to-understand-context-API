cb064050567752de2b4e5bba6595801e32dacdd2
sahebrao jadhav <onlysahebpatil@gmail.com>
2024-04-10 12:51:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      hello guyts 
+    </>
+  )
+}
+
+export default App
